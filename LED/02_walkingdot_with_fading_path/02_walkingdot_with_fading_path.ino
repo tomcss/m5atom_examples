9d7fa62cc6473f7d88d2e0fea58a1954a798f38c
Tom Scheper <scheper@gmail.com>
2020-11-29 15:49:28 +0100

    Added comments

--- LED/02_walkingdot_with_fading_path/02_walkingdot_with_fading_path.ino
+++ LED/02_walkingdot_with_fading_path/02_walkingdot_with_fading_path.ino
@@ -1,12 +1,11 @@
 #include "M5Atom.h"
 
-int shadow_lights[25];
+int shadow_lights[25]; // This will hold our fading path data
 
 void setup()
 {
     M5.begin(true, false, true);
     delay(50);
-    M5.dis.drawpix(0, 0xf00000);
 
     for( int i=0; i<25; i++) {
         shadow_lights[i] = 0;
@@ -17,7 +16,6 @@ uint8_t FSM = 0;
 int xpos = 0;
 int ypos = 0;
 
-
 void draw_shadow() {
     for(int i=0; i<25; i++) {
         M5.dis.drawpix( i, shadow_lights[i]*256);
@@ -26,7 +24,7 @@ void draw_shadow() {
 
 void fade_shadow() {
     for(int i=0; i<25; i++) {
-        shadow_lights[i] *= 0.9;
+        shadow_lights[i] *= 0.9; // Reducing the path brightness by 10% every tick
         if( shadow_lights[i]<40) {
             shadow_lights[i] = 0;
 
@@ -35,19 +33,17 @@ void fade_shadow() {
 }
 
 void move_dot() {
+
     int mov = random(2);
     if( mov == 0) mov = -1;
+
     if( random(2)==1) { // move horizontally
         if( (xpos+mov>=0) && (xpos+mov<=4)) xpos += mov;
-    } else { // move vertically
+    } else {            // move vertically
         if( (ypos+mov>=0) && (ypos+mov<=4)) ypos += mov;
     }
 
-    int light = shadow_lights[ xpos+ypos*5] + 40;
-
-    if( light>255) light = 255;
-
-    shadow_lights[ xpos+ypos*5] = 200;
+    shadow_lights[ xpos+ypos*5] = 200; // Setting the path brightness to 200
 }
 
 void loop()
