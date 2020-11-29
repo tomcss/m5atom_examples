f52acd938a26215763001bdcd239d251b48c0445
Tom Scheper <scheper@gmail.com>
2020-11-29 15:42:14 +0100

    Added comments

--- LED/01_walkingdot/m5atom.ino
+++ LED/01_walkingdot/m5atom.ino
@@ -1,5 +1,10 @@
 #include "M5Atom.h"
 
+/*
+ * Draw a dot that moves one step horizontally or vertically
+ * every tick
+ */
+
 void setup()
 {
     M5.begin(true, false, true);
@@ -14,18 +19,18 @@ int ypos = 0;
 void loop()
 {
 
-    M5.dis.drawpix(xpos+ypos*5, 0);
+    M5.dis.drawpix(xpos+ypos*5, 0); // Clear the dot position LED
 
     int mov = random(2);
     if( mov == 0) mov = -1;
     if( random(2)==1) { // move horizontally
         if( (xpos+mov>=0) && (xpos+mov<=4)) xpos += mov;
-    } else { // move vertically
+    } else {            // move vertically
         if( (ypos+mov>=0) && (ypos+mov<=4)) ypos += mov;
     }
 
-    M5.dis.drawpix(xpos+ypos*5, 0xf00000);
+    M5.dis.drawpix(xpos+ypos*5, 0xf00000); // Draw the dot
     
-    delay(50);
+    delay(50); // Tick
     M5.update();
 }
